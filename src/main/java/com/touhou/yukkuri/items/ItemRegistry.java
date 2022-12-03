4de24c2ab8c1055f709729972b745afd845c7c84
sora <34794115+sorae42@users.noreply.github.com>
2022-12-03 15:41:25 +0700

    cleanup

--- src/main/java/com/touhou/yukkuri/items/ItemRegistry.java
+++ src/main/java/com/touhou/yukkuri/items/ItemRegistry.java
@@ -1,20 +1,9 @@
 package com.touhou.yukkuri.items;
 
-import com.touhou.yukkuri.Yukkuri;
-import com.touhou.yukkuri.entity.EntityRegistry;
 import com.touhou.yukkuri.items.types.Foods;
 import com.touhou.yukkuri.items.types.SpawnEggs;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.entity.EntityType;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.SpawnEggItem;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ItemRegistry {
-
-
 
     public static void init() {
         Foods.init();
