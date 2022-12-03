1598551af34803d0d1ac09e2adcf3b62219694f6
sora <34794115+sorae42@users.noreply.github.com>
2022-12-04 01:29:34 +0700

    inherits from a single class for easier maintainance

--- src/main/java/com/touhou/yukkuri/entity/characters/BlueReimuEntity.java
+++ src/main/java/com/touhou/yukkuri/entity/characters/BlueReimuEntity.java
@@ -1,57 +1,15 @@
 package com.touhou.yukkuri.entity.characters;
 
 import com.touhou.yukkuri.entity.EntityRegistry;
-import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.LocalDifficulty;
-import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.PlayState;
-import software.bernie.geckolib3.core.controller.AnimationController;
-import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
-import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class BlueReimuEntity extends PassiveEntity implements IAnimatable {
-    private AnimationFactory factory = GeckoLibUtil.createFactory(this);
-    private static final TrackedData<String> CHARACTER = DataTracker.registerData(BlueReimuEntity.class, TrackedDataHandlerRegistry.STRING);
-
+public class BlueReimuEntity extends DefaultYukkuri implements IAnimatable {
     public BlueReimuEntity(EntityType<? extends PassiveEntity> entityType, World world) {
         super(entityType, world);
-    }
-
-    public static DefaultAttributeContainer.Builder setAttributes() {
-        return DefaultYukkuri.yukkuriAttribute;
-    }
-
-    @Override
-    protected void initGoals() {
-        this.goalSelector.add(0, new EscapeDangerGoal(this, 1.25D));
-        this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new LookAroundGoal(this));
-        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 8f));
-        this.goalSelector.add(2, new WanderAroundGoal(this, 0.25D));
-    }
-
-    protected void initDataTracker() {
-        super.initDataTracker();
-    }
-
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        return PlayState.CONTINUE;
     }
 
     @Override
@@ -59,19 +17,4 @@ public class BlueReimuEntity extends PassiveEntity implements IAnimatable {
         return EntityRegistry.BLUEREIMU_YUKKURI_TYPE.create(world);
     }
 
-    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
-        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
-    }
-
-    @Override
-    public void registerControllers(AnimationData animationData) {
-        animationData.addAnimationController(
-                new AnimationController<>(this, "controller", 0, this::predicate)
-        );
-    }
-
-    @Override
-    public AnimationFactory getFactory() {
-        return factory;
-    }
 }
--- src/main/java/com/touhou/yukkuri/entity/characters/DefaultYukkuri.java
+++ src/main/java/com/touhou/yukkuri/entity/characters/DefaultYukkuri.java
@@ -1,12 +1,75 @@
 package com.touhou.yukkuri.entity.characters;
 
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class DefaultYukkuri {
+public class DefaultYukkuri extends PassiveEntity implements IAnimatable {
     public static final DefaultAttributeContainer.Builder yukkuriAttribute= PassiveEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 100.00)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5f);
+                    .add(EntityAttributes.GENERIC_MAX_HEALTH, 100.00)
+                    .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5f);
+    private AnimationFactory factory = GeckoLibUtil.createFactory(this);
 
+    public DefaultYukkuri(EntityType<? extends PassiveEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    public static DefaultAttributeContainer.Builder setAttributes() {
+        return DefaultYukkuri.yukkuriAttribute;
+    }
+
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(0, new EscapeDangerGoal(this, 1.25D));
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(0, new LookAtEntityGoal(this, MarisaEntity.class, 12f));
+        this.goalSelector.add(1, new LookAroundGoal(this));
+        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 8f));
+        this.goalSelector.add(2, new WanderAroundGoal(this, 0.25D));
+    }
+
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        return PlayState.CONTINUE;
+    }
+
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+    }
+
+    @Nullable
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
+
+    @Override
+    public void registerControllers(AnimationData animationData) {
+        animationData.addAnimationController(
+                new AnimationController<>(this, "controller", 0, this::predicate)
+        );
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return factory;
+    }
 }
+
