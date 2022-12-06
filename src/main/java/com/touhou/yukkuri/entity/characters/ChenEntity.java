8dfecb1d8278e3aa2f82685d7057dd5b0e851cdb
sora <34794115+sorae42@users.noreply.github.com>
2022-12-06 23:55:46 +0700

    chen now also look at yukari

--- src/main/java/com/touhou/yukkuri/entity/characters/ChenEntity.java
+++ src/main/java/com/touhou/yukkuri/entity/characters/ChenEntity.java
@@ -2,6 +2,7 @@ package com.touhou.yukkuri.entity.characters;
 
 import com.touhou.yukkuri.entity.EntityRegistry;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
@@ -10,6 +11,10 @@ import software.bernie.geckolib3.core.IAnimatable;
 public class ChenEntity extends DefaultYukkuri implements IAnimatable {
     public ChenEntity(EntityType<? extends PassiveEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    protected void initCustomGoals() {
+        this.goalSelector.add(0, new LookAtEntityGoal(this, YukariEntity.class, 12f));
     }
 
     @Override
