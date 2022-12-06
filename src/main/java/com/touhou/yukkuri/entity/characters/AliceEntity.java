1076b96ba0d0633b0a11dfae8fbdb2981e88f494
sora <34794115+sorae42@users.noreply.github.com>
2022-12-06 23:27:16 +0700

    alice now look at marisa

--- src/main/java/com/touhou/yukkuri/entity/characters/AliceEntity.java
+++ src/main/java/com/touhou/yukkuri/entity/characters/AliceEntity.java
@@ -2,6 +2,7 @@ package com.touhou.yukkuri.entity.characters;
 
 import com.touhou.yukkuri.entity.EntityRegistry;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
@@ -10,6 +11,11 @@ import software.bernie.geckolib3.core.IAnimatable;
 public class AliceEntity extends DefaultYukkuri implements IAnimatable {
     public AliceEntity(EntityType<? extends PassiveEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    protected void initCustomGoals() {
+        this.goalSelector.add(0, new LookAtEntityGoal(this, MarisaEntity.class, 12f));
     }
 
     @Override
