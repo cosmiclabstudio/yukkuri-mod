547f539552e57d5c15f0f429a93e90ee88d8c49a
Sora <34794115+sorae42@users.noreply.github.com>
2024-04-22 19:08:45 +0700

    fix(cirno): readd cirno noise

--- src/main/java/com/touhou/yukkuri/entity/characters/CirnoEntity.java
+++ src/main/java/com/touhou/yukkuri/entity/characters/CirnoEntity.java
@@ -1,9 +1,15 @@
+/*
+ * Copyright (c) 2024.
+ */
+
 package com.touhou.yukkuri.entity.characters;
 
 import com.touhou.yukkuri.entity.EntityRegistry;
+import com.touhou.yukkuri.sounds.SoundRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 
 public class CirnoEntity extends DefaultYukkuri {
@@ -15,4 +21,10 @@ public class CirnoEntity extends DefaultYukkuri {
     public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return EntityRegistry.CIRNO_YUKKURI_TYPE.create(world);
     }
+    
+    public SoundEvent getAmbientSound() {
+        return SoundRegistry.ENTITY_CIRNO_BAKA;
+    }
+    
+    
 }
