41a9834601b8b06c00e06fc73693356224167852
Sora <34794115+sorae42@users.noreply.github.com>
2024-04-24 16:55:14 +0700

    feat(youmu): wip animation

--- src/main/java/com/touhou/yukkuri/entity/characters/YoumuEntity.java
+++ src/main/java/com/touhou/yukkuri/entity/characters/YoumuEntity.java
@@ -7,10 +7,18 @@ import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class YoumuEntity extends DefaultYukkuri {
-    // TODO: the animation doesn't work so i just comment them out for now
-    //private final AnimationBuilder FLOATING_GHOST = new AnimationBuilder().addAnimation("animation.youmu.ghost_guided", ILoopType.EDefaultLoopTypes.LOOP);
+public class YoumuEntity extends DefaultYukkuri implements IAnimatable {
+    private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
+    private final AnimationBuilder FLOATING_GHOST = new AnimationBuilder().loop("animation.youmu.ghost_guided");
 
     public YoumuEntity(EntityType<? extends PassiveEntity> entityType, World world) {
         super(entityType, world);
@@ -20,7 +28,24 @@ public class YoumuEntity extends DefaultYukkuri {
     public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return EntityRegistry.YOUMU_YUKKURI_TYPE.create(world);
     }
+    
+    protected <E extends YoumuEntity> PlayState predicate(final AnimationEvent<E> event) {
+        event.getController().setAnimation(FLOATING_GHOST);
+        return PlayState.CONTINUE;
+    }
 
+    @Override
+    public void registerControllers(AnimationData animationData) {
+        animationData.addAnimationController(
+                new AnimationController<>(this, "controller", 0, this::predicate)
+        );
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return factory;
+    }
+    
     @Override
     protected SoundEvent getAmbientSound() {
         return SoundRegistry.ENTITY_YOUMU_MYON;
